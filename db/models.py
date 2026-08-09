aef5e707de7fa0ccabd5810b69712e793df6d304
nik <Nick1t0s@yandex.ru>
2026-08-09 13:42:27 +0300

    Limit DB pool to a single connection

--- db/models.py
+++ db/models.py
@@ -100,7 +100,7 @@ class NotificationLog(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utcnow)
 
 
-engine = create_async_engine(DB_URL)
+engine = create_async_engine(DB_URL, pool_size=1, max_overflow=0)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
